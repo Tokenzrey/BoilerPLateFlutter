27c8be8433cffb275e74051184774f6c04c57357
danishaustinwicaksana <5025221165@student.its.ac.id>
2025-06-12 16:47:20 +0700

    Navbar Atas & Bawah Done

--- lib/core/widgets/search/popover_menu.dart
+++ lib/core/widgets/search/popover_menu.dart
@@ -5,28 +5,62 @@ class PopoverMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      leading: IconButton(
-        icon: Image.network(
-          'https://comick.io/static/icons/55x55-icon.png'
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    final textColor = isDark ? Colors.white70 : Colors.black87;
+
+    return PopupMenuButton<int>(
+      icon: Icon(Icons.account_circle_rounded, color: textColor),
+      shadowColor: isDark ? Colors.grey[200] : Colors.black54,
+      itemBuilder: (context) => [
+        PopupMenuItem(
+          value: 1,
+          child: Row(
+            children: [
+              Icon(Icons.accessibility_new, color: textColor),
+              const SizedBox(
+                width: 10,
+              ),
+              Text("Profile", style: TextStyle(color: textColor) ),
+            ],
+          ),
         ),
-        onPressed: () {},
-      ),
-      backgroundColor: Colors.white,
-      actions: [
-        IconButton(
-          icon: Icon(Icons.search),
-          onPressed: () {
-            // Handle search action
-          },
+        PopupMenuItem(
+          value: 2,
+          child: Row(
+            children: [
+              Icon(Icons.settings, color: textColor),
+              const SizedBox(
+                width: 10,
+              ),
+              Text("Settings", style: TextStyle(color: textColor),)
+            ],
+          ),
         ),
-        IconButton(
-          icon: Icon(Icons.account_circle),
-          onPressed: () {
-            // Handle account action
-          },
+        PopupMenuItem(
+          value: 3,
+          child: Row(
+            children: [
+              Icon(Icons.logout, color: textColor),
+              const SizedBox(
+                width: 10,
+              ),
+              Text("Logout", style: TextStyle(color: textColor) ),
+            ],
+          ),
         ),
       ],
+      offset: Offset(0, 52),
+      color: Theme.of(context).colorScheme.surface,
+      elevation: 2,
+      onSelected: (value) {
+        if (value == 1) {
+          // _showDialog(context);
+        } else if (value == 2) {
+          // _showDialog(context);
+        } else if (value == 3) {
+          // _showDialog(context);
+        }
+      },
     );
   }
 }
